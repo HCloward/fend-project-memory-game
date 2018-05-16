566c9af21b45b91deb5f2fd3ac67ac0809292c86
Heather Cloward <heathercloward@gmail.com>
2018-05-16 15:56:16 -0400

    Cleaned up code a bit.

--- js/app.js
+++ js/app.js
@@ -1,16 +1,4 @@
 /*
- * Create a list that holds all of your cards
- * Used getElementsByClassName as it has a faster load time than querySelector and it produces an html collection instead of a generic node list. 
- * The html collection will only contain any node element, which is more specific than a node list.
- */
-
-
-/* Used destructuring here to grab the items in the node list created above and place them into an array 
-*so that that array can be looped over and it can be used in the shuffle function.*/
-
-
-
-/*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
  *   - loop through each card and create its HTML
@@ -50,7 +38,6 @@ function startNewGame() {
     cardDeck.innerHTML = cardHTML;
 }
  startNewGame();
-
     
     // Shuffle function from http://stackoverflow.com/a/2450976
     function shuffle(array) {
@@ -66,12 +53,6 @@ function startNewGame() {
     
         return array;
     }
-
-
- 
-
-
-
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
@@ -84,16 +65,26 @@ function startNewGame() {
  */
 
 
- /* Used a function expression here instead of a declaration since I am using the function as a callback inside of the for loop below. 
- *Used the "this" keyword to reference the individual cards in the for loop below. */
+ 
 
+/*
+ * Create a list that holds all of your cards
+ * Used getElementsByClassName as it has a faster load time than querySelector and it produces an html collection instead of a generic node list. 
+ * The html collection will only contain any node element, which is more specific than a node list.
+ */
 let cardList = document.getElementsByClassName('card');
+
+/* Used destructuring here to grab the items in the node list created above and place them into an array 
+*so that that array can be looped over and it can be used in the shuffle function.*/
+
 let allCards = [...cardList];
+
+/* Used a function expression here instead of a declaration since I am using the function as a callback inside of the for loop below. 
+ *Used the "this" keyword to reference the individual cards in the for loop below. */
 
 const displayCard = function() {
     this.classList.toggle("open");
     this.classList.toggle("show");
-    this.classList.toggle("disabled");
 };
 
 /*
